5aadae9b4c59fde357764c53e205ea6e9f22a41e
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-09 19:11:34 -0700

    Added relationship and rating validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,11 @@
 class Review < ApplicationRecord
+  belongs_to :product
+
+  validates :rating, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+
+# validates NOT merchant id
 end
