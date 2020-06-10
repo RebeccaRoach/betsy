df22e40754b7e29615c27e67a01abfc75437b211
denisseai <msdenisseanaya@gmail.com>
2020-06-09 21:59:43 -0700

    Added orderitem model

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -1,2 +1,23 @@
 class Orderitem < ApplicationRecord
+  belongs_to :product
+  belongs_to :order
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
+  validates :shipped, inclusion: { in: [true, false],message: "shipped status : true or false"}
+
+  # validate :in_stock
+  # validate :not_retired
+
+  # def subtotal
+  #
+  # end
+
+  private
+  # def in_stock
+  # 
+  # end
+
+  # def not_retired
+  #
+  # end
 end
