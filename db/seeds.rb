b16af7d7efad1da0810b9e2c4b7d667b1eb6c6bd
dnguye2 <dnguyen1093@gmail.com>
2020-06-10 13:26:50 -0700

    added function to seed data from 'products.csv'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 require "csv"
-media_file = Rails.root.join("db", "products.csv")
+products_file = Rails.root.join("db", "products.csv")
 
-CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+CSV.foreach(products_file, headers: true, header_converters: :symbol, converters: :all) do |row|
   data = Hash[row.headers.zip(row.fields)]
   puts data
   Product.create!(data)
 end
-
