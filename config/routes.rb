cecd6f2453200cb81a3ed039db4e11e7ec8f1d50
denisseai <msdenisseanaya@gmail.com>
2020-06-09 18:38:17 -0700

    Added routes for orders and orderitems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homepages#index'
+  
+  resources :orderitems, only: [:edit, :destroy]
+  patch 'orderitems/:id', to: 'orderitems#update'
+
+  resources :orders, only: [:show, :edit]
+  get '/cart', to: 'orders#cart', as: 'cart'
+  patch '/orders/:id', to: 'orders#update'
 end
