1149e4f45b7d8ed3470cb586fa01e96b0c9263e3
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-10 10:23:30 -0700

    Changed homepages to pages in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homepages#index'
+  root to: 'pages#index'
 
   resources :orderitems, only: [:edit, :destroy]
   patch 'orderitems/:id', to: 'orderitems#update'
