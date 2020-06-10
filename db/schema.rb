1fcb89a7c89aa6690a8eed47db507bf0cec8023e
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-10 11:22:48 -0700

    Updated schema to account for products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_063347) do
+ActiveRecord::Schema.define(version: 2020_06_10_071324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2020_06_10_063347) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.string "description"
+    t.float "price"
+    t.string "photo_url"
+    t.bigint "available_quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
