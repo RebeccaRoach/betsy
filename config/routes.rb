e1bc31617efd33b92b907c78f4d09ef0b5156b80
denisseai <msdenisseanaya@gmail.com>
2020-06-09 21:42:19 -0700

    Added shipped to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homepages#index'
-  
+
   resources :orderitems, only: [:edit, :destroy]
   patch 'orderitems/:id', to: 'orderitems#update'
+  patch 'orderitems/:id/mark_shipped', to: 'orderitems#markshipped', as: 'mark_shipped'
 
   resources :orders, only: [:show, :edit]
   get '/cart', to: 'orders#cart', as: 'cart'
