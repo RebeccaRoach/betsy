67678aa4dd3bcdbf528786e138842a365ba1824b
dnguye2 <dnguyen1093@gmail.com>
2020-06-10 15:43:06 -0700

    changed product attribute "available_quantity" to "stock"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_211317) do
+ActiveRecord::Schema.define(version: 2020_06_10_223722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_211317) do
     t.string "description"
     t.float "price"
     t.string "photo_url"
-    t.bigint "available_quantity"
+    t.bigint "stock"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
