590f5e767456be575737b608fae98228ea515aee
denisseai <msdenisseanaya@gmail.com>
2020-06-09 18:40:05 -0700

    Added methods cart, update for orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,50 @@
+class OrdersController < ApplicationController
+  # product model made then collab for update order quantity
+
+  # Find order or display message if cart is empty
+  def cart
+    if session[:order_id]
+      @order = Order.find_by(id: session[:order_id])
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Cart is empty"
+      redirect_back fallback_location: root_path
+      return
+    end
+  end
+
+  # Process order after payment info has been addded
+  def update
+    @order.orderitems.each do |oderitem|
+      if !orderitem.valid?
+        flash[:status] = :failure
+        flash[:result_text] = "Some items in your cart are no longer available"
+        flash[:messages] = orderitem.errors.messages
+      end
+      if flash[:status] == :failure
+        return redirect_to cart_path
+      end
+    end
+
+    # Change status and clear cart 
+    @order.status = "paid"
+    if @order.update(order_params)
+      @order.reduce_stock
+      session[:order_id] = nil
+      redirect_to order_path(@order.id)
+      return
+    else
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Review and Resubmit"
+      flash.now[:messages] = orderitem.errors.messages
+
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:email, :mailing_address, :cc_number, :cvv, :expiration, :zip)
+  end
+end
