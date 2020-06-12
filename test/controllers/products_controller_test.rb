aa557c6421b0abd600b384dc67910b11de32eba9
dnguye2 <dnguyen1093@gmail.com>
2020-06-11 21:54:47 -0700

    products#index tests created and pass

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @invalid_product_id = -1
+  end
+
+  describe "index" do
+    it "responds with success when there are many passengers saved" do
+      Product.create(product_name: 'Smartwool Socks', price: 20.00)
+      Product.create(product_name: 'Cliff Coffee Bar', price: 2.00)
+
+      get "/products"
+
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no passengers saved" do
+      get "/products"
+
+      must_respond_with :success
+    end
+  end
 end
