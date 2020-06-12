dbb580c65839a421a7dd6a91924f25da7c49f2e4
denisseai <msdenisseanaya@gmail.com>
2020-06-11 21:05:51 -0700

    testing

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe OrderitemsController do
-  let(:existing_orderitem) { orderitems(:snow) }
+  let(:existing_orderitem) { orderitems(:rxbar) }
 
   let(:update_hash){
     {
@@ -14,14 +14,14 @@ describe OrderitemsController do
   describe "create function" do
     it "can create a new Orderitem with valid data" do
       expect {
-        post product_orderitems_path(product_id: products(:rx_bar).id), params: update_hash
+        post product_orderitems_path(product_id: products(:cliff).id), params: update_hash
       }.must_change "Orderitem.count", 1
 
       expect(session[:order_id]).wont_be_nil
 
       new_orderitem = Orderitem.last
 
-      expect(new_orderitem.product.product_name).must_equal "RxBar"
+      expect(new_orderitem.product.name).must_equal "Cliff"
       expect(new_orderitem.order.orderitems.count).must_equal 1
       must_respond_with :redirect
     end
