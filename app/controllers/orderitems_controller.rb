a734fd11ce085c67d71beefa411bc1edf0539a56
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-14 22:30:03 -0700

    Added return in #create, added controller tests for all but #mark_shipped, fixed routes to reflect nesting in orders

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,7 +1,8 @@
 class OrderitemsController < ApplicationController
-  before_action :find_orderitem, only: [:edit, :update, :destroy, :mark_shipped]
+  before_action :find_orderitem, only: [:update, :destroy, :mark_shipped]
 
-  # create a new order or add to existing- do we want this?
+  # create a new orderitem
+  # add to existing order if order exists; if not, create new order and add this orderitem to it
   def create
     if session[:order_id] && Order.find_by(id: session[:order_id])
       @order = Order.find_by(id: session[:order_id])
@@ -10,7 +11,8 @@ class OrderitemsController < ApplicationController
       unless @order.save
         flash[:status] = :failure
         flash[:result_text] = "Couldn't create order, try again later"
-        flash[:messages] = orderitem.errors.messages
+        # flash[:messages] = orderitem.errors.messages
+        return
       end
       session[:order_id] = @order.id
     end
@@ -20,6 +22,7 @@ class OrderitemsController < ApplicationController
     if @orderitem
       @orderitem.quantity += params[:orderitem][:quantity].to_i
     else
+    # need to create new Orderitem
       @orderitem = Orderitem.new(
         quantity: params[:orderitem][:quantity],
         product_id: params[:product_id],
@@ -30,31 +33,29 @@ class OrderitemsController < ApplicationController
 
     if @orderitem.save
       flash[:status] = :success
-      flash[:result_text] = "#{orderitem.product.name} was added to cart"
+      flash[:result_text] = "Added #{@orderitem.product.product_name} to cart!"
     else
       flash[:status] = :failure
-      flash[:result_text] = "Error adding #{orderitem.product.name} to cart"
+      flash[:result_text] = "Error adding #{@orderitem.product.product_name} to cart."
       flash[:messages] = @orderitem.errors.messages
     end
-    redirect_back fallback_location: root_path
-    return
-  end
 
-  def edit ; end
+    # continue shopping at same point or go back to root?
+    redirect_to cart_path(id: session[:order_id])
+  end
   
-  # comment here
   def update
     if @orderitem.order.status == "pending"
       if @orderitem.update(quantity: params[:orderitem][:quantity])
         flash[:status] = :success
-        flash[:result_text] = "Update sucessful"
+        flash[:result_text] = "Update successful"
         redirect_to cart_path
         return
       else
         flash.now[:status] = :failure
         flash.now[:result_text] = "Error updating the item"
         flash.now[:messages] = @orderitem.errors.messages
-        render :edit, status: :bad_request
+        redirect_to cart_path
         return
       end
     else
@@ -64,13 +65,13 @@ class OrderitemsController < ApplicationController
     end
   end
 
-  # comment here
+
   def destroy
     if @orderitem.order.status == "pending"
       @orderitem.destroy
       flash[:status] = :success
-      flash[:result_text] = "#{@orderitem.product.name} was removed from your cart"
-      redirect_to cart_path
+      flash[:result_text] = "#{@orderitem.product.product_name} was removed from your cart"
+      redirect_to cart_path(order_id: session[:id])
     else
       flash[:status] = :failure
       flash[:result_text] = "Cannot delete items"
@@ -78,14 +79,16 @@ class OrderitemsController < ApplicationController
     end
   end
 
-  # comment here
+
   def mark_shipped
+    # put into model test
     if @orderitem.order.status == "paid" && @orderitem.shipped == false
       @orderitem.shipped = true
       @orderitem.save
       flash[:status] = :success
-      flash[:result_text] = "#{@orderitem.product.name} - shipped"
-      @orderitem.order.mark_as_complete?
+      flash[:result_text] = "#{ @orderitem.product.product_name } - shipped"
+      # @orderitem.order.mark_as_complete!
+
       redirect_back fallback_location: root_path
 
     elsif @orderitem.order.status == "paid" && @orderitem.shipped == true
--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -3,27 +3,156 @@ require "test_helper"
 describe OrderitemsController do
   let(:existing_orderitem) { orderitems(:rxbar) }
 
-  let(:update_hash){
+  let(:update_hash) {
     {
       orderitem: {
-        quantity: 1,
-      },
+        quantity: 1
+      }
     }
   }
 
-  describe "create function" do
-    it "can create a new Orderitem with valid data" do
-      expect {
-        post product_orderitems_path(product_id: products(:cliff).id), params: update_hash
-      }.must_change "Orderitem.count", 1
-
+  describe "create" do
+    it "can create a new orderitem with valid data" do
+      before_count = Orderitem.count
+      post product_orderitems_path(product_id: products(:snow_pass).id), params: update_hash
+    
+      expect(Orderitem.count).must_equal before_count + 1
       expect(session[:order_id]).wont_be_nil
 
-      new_orderitem = Orderitem.last
+      last_orderitem = Orderitem.last
 
-      expect(new_orderitem.product.name).must_equal "Cliff"
-      expect(new_orderitem.order.orderitems.count).must_equal 1
+      expect(last_orderitem.product).must_equal products(:snow_pass)
+      expect(last_orderitem.order.orderitems.count).must_equal 1
       must_respond_with :redirect
+      must_redirect_to cart_path(last_orderitem.order)
     end
+
+    it "can increment the quantity of existing orderitem" do
+      product_to_buy = products(:snow_pass)
+
+      params = {
+        orderitem: {
+          quantity: 1,
+          shipped: false
+        }
+      }
+
+      before_count_orderitem = Orderitem.count
+      before_count_order = Order.count
+
+      post product_orderitems_path(product_id: products(:snow_pass).id), params: params
+      expect(Order.count).must_equal before_count_order + 1
+
+      post product_orderitems_path(product_id: products(:snow_pass).id), params: params
+      expect(Order.count).must_equal before_count_order + 1
+  
+      expect(Orderitem.count).must_equal before_count_orderitem + 1
+      expect(Orderitem.last.quantity).must_equal 2
+    end
+
+    it "does something if the product is out of stock????" do
+      # create product with 0 stock
+      # expect create post will return 400
+      # should not create orderitem (check same count)
+    end
+  end
+
+  describe "update" do
+    it "succeeds for valid data and an extant orderitem ID" do
+      expect {
+        patch orderitem_path(existing_orderitem), params: update_hash
+      }.wont_change "Orderitem.count"
+      
+      updated_item = Orderitem.find_by(id: existing_orderitem.id)
+
+      expect(updated_item.quantity).must_equal 1
+      must_respond_with :redirect
+      must_redirect_to cart_path(order_id: session[:order_id])
+    end
+
+    it "redirects to cart path without updating given bad data" do
+      bogus_update_hash = {
+        orderitem: {
+          quantity: -1
+        }
+      }
+
+      expect {
+        patch orderitem_path(existing_orderitem), params: bogus_update_hash
+      }.wont_change "Orderitem.count"
+
+      must_respond_with :redirect
+      must_redirect_to cart_path(order_id: session[:order_id])
+    end
+
+    it "redirects to cart path without updating given valid data but not pending status" do
+      # DO WE NEED TO CHECK OTHER STATUSES AS WELL????
+      update_hash = {
+        orderitem: {
+          quantity: 1
+        }
+      }
+
+      existing_orderitem.order.update!(status: "complete")
+      before_quantity = existing_orderitem.quantity
+
+      expect {
+        patch orderitem_path(existing_orderitem), params: update_hash
+      }.wont_change "Orderitem.count"
+
+      expect(existing_orderitem.quantity).must_equal before_quantity
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it "responds with 404 when orderitem does not exist" do
+      orderitem = orderitems(:rxbar)
+      orderitem.destroy!
+      patch orderitem_path(orderitem), params: update_hash
+      must_respond_with :not_found
+    end
+  end
+
+  describe "destroy" do
+    it "deletes item for existing orderitem with pending status, and redirects" do
+      before_count = Orderitem.count
+
+      delete orderitem_path(existing_orderitem)
+
+      expect(Orderitem.count).must_equal before_count - 1
+      must_redirect_to cart_path(order_id: session[:order_id])
+    end
+
+    it "fails to delete existing orderitem without an order status of pending, and redirects" do
+      # DO WE NEED TO CHECK OTHER STATUSES AS WELL????
+      before_count = Orderitem.count
+      existing_orderitem.order.status = "complete"
+      existing_orderitem.order.save!
+
+      delete orderitem_path(existing_orderitem)
+
+      expect(Orderitem.count).must_equal before_count
+      must_redirect_to root_path
+    end
+
+    it "responds with 404 when orderitem does not exist" do
+      orderitem = orderitems(:rxbar)
+      orderitem.destroy!
+      delete orderitem_path(orderitem)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "mark_shipped" do
+    it "can correctly change shipped status for existing orderitem" do
+    end
+
+    # it "responds with 404 when orderitem does not exist" do
+    #   orderitem = orderitems(:rxbar)
+    #   orderitem.destroy!
+    #   # change to right path::: delete orderitem_path(orderitem)
+    #   must_respond_with :not_found
+    # end
   end
 end
