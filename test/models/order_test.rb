2bbfbfb59316ead91ff4d684a3a64715cfe1369b
denisseai <msdenisseanaya@gmail.com>
2020-06-14 18:11:58 -0700

    Added tests to models/order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -14,7 +14,7 @@ describe Order do
     end
   end
 
-  it "cannot have a status other than pending, paid or complete" do
+  it "cannot have a status other than pending, paid, cancel or complete" do
     paid_order.status = "pending"
     expect(paid_order.valid?).must_equal true
 
@@ -23,6 +23,10 @@ describe Order do
 
     paid_order.status = "complete"
     expect(paid_order.valid?).must_equal true
+
+    #why is cancel not working
+    # paid_order.status = "cancel"
+    # expect(paid_order.valid?).must_equal true
 
     paid_order.status = "gretchen"
     expect(paid_order.valid?).must_equal false
@@ -83,6 +87,130 @@ describe Order do
     expect(updated_order.cc_exp).must_be_nil
     expect(updated_order.zip).must_be_nil
     expect(updated_order.status).must_equal "pending"
+  end
+
+  it "will outputs errors if updating Orders without orderitems" do
+    updated_order = pending_order.update(
+      email: "lady@gmail.com",
+      address: "2020 my house street",
+      cc_name: "Butterfly Cherry",
+      cc_num: 41658214563,
+      cvv: 589,
+      cc_exp: "02/2023",
+      zip: 98028,
+      status: "paid"
+    )
+    expect(updated_order).must_equal false
+    expect(pending_order.errors.messages).must_include :orderitems
+    expect(pending_order.errors.messages[:orderitems]).must_include "Your cart is empty"
+  end
+
+  #low hanging fruit :P
+  describe "All attributes must be present when updating" do
+    before do
+      expect{ Orderitem.create(quantity: 1, order: pending_order, product: products(:rainer), shipped: false) }.must_change "pending_order.orderitems.count", 1
+      
+      @updated_order = pending_order.update(
+        status: "paid"
+      )
+    end
     
+    it "will not update an Order without an email" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :email
+      expect(pending_order.errors.messages[:email]).must_include "can't be blank"
+    end
+
+    it "will not update an Order without an address" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :address
+      expect(pending_order.errors.messages[:address]).must_include "can't be blank"
+    end
+
+    it "will not update an Order without a cc_name" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :cc_name
+      expect(pending_order.errors.messages[:cc_name]).must_include "can't be blank"
+    end
+
+    it "will not update an Order without a cc_num" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :cc_num
+      expect(pending_order.errors.messages[:cc_num]).must_include "can't be blank"
+    end
+
+    it "cannot update an Order with a non-numerical cc_num" do
+      updated_order = paid_order.update(cc_num: "abc")
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :cc_num
+      expect(paid_order.errors.messages[:cc_num]).must_include "is not a number"
+    end
+
+    it "cannot update an Order with a cc_num that is a float" do
+      updated_order = paid_order.update(cc_num: 123.4)
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :cc_num
+      expect(paid_order.errors.messages[:cc_num]).must_include "must be an integer"
+    end
+
+    it "cannot update an Order with a cc_num with less than 4 digits" do
+      updated_order = paid_order.update(cc_num: 123)
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :cc_num
+      expect(paid_order.errors.messages[:cc_num]).must_include "is too short (minimum is 4 characters)"
+    end 
+
+    it "will not update an Order without a cvv" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :cvv
+      expect(pending_order.errors.messages[:cvv]).must_include "can't be blank"
+    end
+
+    it "cannot update an Order with a cvv that is a float" do
+      updated_order = paid_order.update(cvv: 0.0)
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :cvv
+      expect(paid_order.errors.messages[:cvv]).must_include "must be an integer"
+    end
+
+    it "cannot update an Order with a non-numerical cvv" do
+      updated_order = paid_order.update(cvv: "abc")
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :cvv
+      expect(paid_order.errors.messages[:cvv]).must_include "is not a number"
+    end 
+
+    it "will not update an Order ithout a cc_exp" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :cc_exp
+      expect(pending_order.errors.messages[:cc_exp]).must_include "can't be blank"
+    end
+    
+    it "will not update an Order ithout a zip" do
+      expect(@updated_order).must_equal false
+      expect(pending_order.errors.messages).must_include :zip
+      expect(pending_order.errors.messages[:zip]).must_include "can't be blank"
+    end
+    
+    it "cannot update an Order with a non-numerical zip" do
+      updated_order = paid_order.update(zip: "abc")
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :zip
+      expect(paid_order.errors.messages[:zip]).must_include "is not a number"
+    end 
+    
+    it "cannot update an Order with a zip that is a float" do
+      updated_order = paid_order.update(zip: 0.0)
+      
+      expect(updated_order).must_equal false
+      expect(paid_order.errors.messages).must_include :zip
+      expect(paid_order.errors.messages[:zip]).must_include "must be an integer"
+    end
   end
 end
