719246f75d57f4ef318e812cac0759cb9a096721
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-15 20:07:45 -0700

    Changed add_columns to change_columns in order migration.

--- db/migrate/20200611014346_add_columns_to_order.rb
+++ db/migrate/20200611014346_add_columns_to_order.rb
@@ -1,13 +1,13 @@
 class AddColumnsToOrder < ActiveRecord::Migration[6.0]
   def change
     add_column :orders, :total, :float
-    add_column :orders, :status, :string
-    add_column :orders, :email, :string
-    add_column :orders, :address, :string
-    add_column :orders, :cc_name, :string
-    add_column :orders, :cc_num, :bigint
-    add_column :orders, :cvv, :integer
-    add_column :orders, :cc_exp, :integer
-    add_column :orders, :zip, :integer
+    change_column :orders, :status, :string
+    change_column :orders, :email, :string
+    change_column :orders, :address, :string
+    change_column :orders, :cc_name, :string
+    change_column :orders, :cc_num, :bigint
+    change_column :orders, :cvv, :integer
+    change_column :orders, :cc_exp, :integer
+    change_column :orders, :zip, :integer
   end
 end
