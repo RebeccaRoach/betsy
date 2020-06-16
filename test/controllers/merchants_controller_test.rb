29457af76a77a0080b45daf7e95907d577a4d0af
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-16 09:29:52 -0700

    working on merch_controller tests

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:merchants) { merchants(:bikin_bobby) }
+
+  describe "index" do
+    it "succeeds when there are merchants" do
+      get merchants_path
+
+      must_respond_with :success
+    end
+  end
+
+  
+
+
+
+
+
+
+
+  
 end
