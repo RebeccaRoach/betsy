3ae2386f07bb1841fa91ae97c576a94ad3e379ec
denisseai <msdenisseanaya@gmail.com>
2020-06-18 10:28:54 -0700

    Added yml file for reviews

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 describe ReviewsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  require "test_helper"
+
+  describe Review do
+    describe "new" do
+      it "" do
+        get new_review_path(product_id: Product.first.id)
+        must_respond_with :success
+      end
+    end
+    describe "create" do
+      it "can create a valid review and redirects to product page" do
+        valid_hash = {
+          review: {
+            rating: 5,
+            product: products(:rx_bar)
+          },
+        }
+        expect {
+          post reviews_path(product_id: products(:rx_bar).id), params: valid_hash
+        }.must_change "Review.count", 1
+        
+        new_review = Review.last
+        
+        expect(new_review.rating).must_equal 5
+        expect(new_review.text_review).must_be_nil
+        expect(new_review.product_id).must_equal products(:rx_bar).id
+        
+        must_redirect_to product_path(id: products(:rx_bar).id)
+      end
+    end
+    describe "review_params" do
+    end
+  end
+  
 end
