3716c9377552e59cebf0614f7fe2a046f36f365b
denisseai <msdenisseanaya@gmail.com>
2020-06-18 14:08:43 -0700

    Added test for review controller create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,7 +18,7 @@ class ReviewsController < ApplicationController
 
   private
 
-  def review_params
+  def review_params1
     return params.require(:review).permit(:content, :rating, :product_id)
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -5,34 +5,37 @@ describe ReviewsController do
 
   describe Review do
     describe "new" do
-      it "" do
+      it "successfully retrieve new review page for valid product id" do
         get new_review_path(product_id: Product.first.id)
         must_respond_with :success
       end
     end
+
     describe "create" do
+      let(:product) { products(:rx_bar) }
       it "can create a valid review and redirects to product page" do
         valid_hash = {
           review: {
             rating: 5,
-            product: products(:rx_bar)
+            product_id: product.id
           },
-        }
-        expect {
-          post reviews_path(product_id: products(:rx_bar).id), params: valid_hash
-        }.must_change "Review.count", 1
-        
+        } 
+
+        assert_difference("Review.count") do
+          post reviews_path(product.id), params: valid_hash 
+        end
+
         new_review = Review.last
         
         expect(new_review.rating).must_equal 5
-        expect(new_review.text_review).must_be_nil
         expect(new_review.product_id).must_equal products(:rx_bar).id
+        expect
         
-        must_redirect_to product_path(id: products(:rx_bar).id)
+        must_redirect_to product_path(products(:rx_bar).id)
       end
     end
-    describe "review_params" do
-    end
+    # describe "review_params" do
+    # end
   end
   
 end
