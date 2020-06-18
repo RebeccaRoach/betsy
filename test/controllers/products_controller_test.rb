6eac820d9e4b7018eb0e60af3522dc6d9800d726
dnguye2 <dnguyen1093@gmail.com>
2020-06-18 01:14:36 -0700

    created and passed product#create tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -53,8 +53,67 @@ describe ProductsController do
   end
 
   describe "create" do
-    
+    let (:product_hash) {
+      {
+        product: {
+          product_name: "Mt. Everest",
+          description: "Yeah that mountain",
+          price: 300000,
+          photo_url: "www.learningtoliketests.com/sort-of.png",
+          stock: 3,
+          retired: false,
+          merchant_id: merchants(:diana).id
+        }
+      }
+    }
+
+    it "can create a new product, and responds with a redirect" do
+      expect {
+        post products_path, params: product_hash
+      }.must_differ 'Product.count', 1
+
+      must_redirect_to product_path(Product.last)
+      expect(Product.last.product_name).must_equal product_hash[:product][:product_name]
+    end
+
+    it "does not create a product if given bad data, and responds with a redirect" do
+      
+    end
+
   end
+
+
+  # describe "create" do
+  #   let (:passenger_hash) {
+  #     {
+  #       passenger: {
+  #         name: "Cookie Monster", 
+  #         phone_num: "9999"
+  #       }
+  #     }
+  #   }
+
+    # it "can create a new passenger with valid information accurately, and redirect" do
+    #   expect {
+    #     post passengers_path, params: passenger_hash
+    #   }.must_differ 'Passenger.count', 1
+
+    #   must_redirect_to root_path
+    #   expect(Passenger.last.name).must_equal passenger_hash[:passenger][:name]
+    #   expect(Passenger.last.phone_num).must_equal passenger_hash[:passenger][:phone_num]
+    # end
+
+    # it "does not create a passenger if the form if the form has invalid name, and responds with a redirect" do
+    #   no_name = passenger_hash[:passenger][:name] = nil
+
+    #   expect {
+    #     post passengers_path, params: no_name
+    #   }.must_differ 'Passenger.count', 0
+
+    #   must_respond_with  :redirect
+    #   must_redirect_to new_passenger_path
+    # end
+  # end
 
   describe "update" do
     
