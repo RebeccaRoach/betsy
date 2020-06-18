164b8ae59789828a33c6909f202fe2572921dd16
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-17 21:51:14 -0700

    Added validation on stock for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :product_name, presence: true, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :merchant
   has_and_belongs_to_many :categories
