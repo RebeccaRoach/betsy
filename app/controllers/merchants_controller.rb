81b43fd9bd54f3bbce68472cce1cefaf9dc77877
dnguye2 <dnguyen1093@gmail.com>
2020-06-18 20:11:35 -0700

    changed login and logout messages

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -19,13 +19,13 @@ class MerchantsController < ApplicationController
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
     
     if merchant
-      flash[:result_text] = "Returning merchant: #{merchant.username}"  
+      flash[:result_text] = "Did you get lost in the woods? We missed you, #{merchant.username}"  
     else
       merchant = Merchant.build_from_github(auth_hash)
       if merchant.save
-        flash[:result_text] = "You are logged in as new merchant: #{merchant.username}." 
+        flash[:result_text] = "Welcome to the great Outdoorsy, #{merchant.username}." 
       else
-        flash[:result_text] = "Failure: Could not create new merchant account"
+        flash[:result_text] = "Failure: Take a hike!"
         flash[:messages] = merchant.errors.messages
         return redirect_to root_path
       end 
@@ -37,7 +37,7 @@ class MerchantsController < ApplicationController
 
   def logout
     session[:merchant_id] = nil
-    flash[:result_text] = "Successfully logged out"
+    flash[:result_text] = "Get Outdoorsy, catch ya later alligator"
     redirect_to root_path
     return
   end
